7fcfa540f9934fd38fc5f04e512de17460b35295
Caryn Ligon <carynligon@gmail.com>
2016-10-22 14:08:54 -0500

    styled modal

--- app/scripts/Components/Modal.js
+++ app/scripts/Components/Modal.js
@@ -16,7 +16,6 @@ class Modal extends React.Component {
       email: this.refs.email.value,
       password: this.refs.password.value
     }
-    console.log(loginInfo);
   }
   signup(e) {
     e.preventDefault();
@@ -27,7 +26,6 @@ class Modal extends React.Component {
       email: this.refs.email.value,
       password: this.refs.password.value
     }
-    console.log(signupInfo);
   }
   containerStyles() {
     return ({
@@ -44,7 +42,7 @@ class Modal extends React.Component {
     return ({
       position: 'relative',
       height: '50%',
-      width: '50%',
+      width: '450px',
       background: '#F45D01',
       margin: '20% auto'
     });
@@ -53,28 +51,62 @@ class Modal extends React.Component {
     console.log(this.props);
     let form = (
       <form className="signup-form" onSubmit={this.signup.bind(this)}>
-        <label htmlFor="first-name">FIRST NAME</label>
-        <input type="text" id="first-name" ref="firstName"/>
-        <label htmlFor="last-name">LAST NAME</label>
-        <input type="text" id="last-name" ref="lastName"/>
-        <label htmlFor="zipcode">ZIPCODE</label>
-        <input type="number" id="zipcode" ref="zipcode" pattern='[0-9]{5}'/>
-        <label htmlFor="email">EMAIL</label>
-        <input type="email" id="email"/>
-        <label htmlFor="password">PASSWORD</label>
-        <input type="password" id="password"/>
-        <label htmlFor="password-confirm">CONFIRM PASSWORD</label>
-        <input type="password" id="password-confirm" ref="confirmPassword"/>
+
+        <h3>SIGNUP</h3>
+
+        <div className="signup-group1">
+          <div className="input-wrapper">
+            <label htmlFor="first-name">FIRST NAME</label>
+            <input type="text" id="first-name" ref="firstName" required/>
+          </div>
+
+          <div className="input-wrapper">
+            <label htmlFor="last-name">LAST NAME</label>
+            <input type="text" id="last-name" ref="lastName" required/>
+          </div>
+
+          <div className="input-wrapper">
+            <label htmlFor="zipcode">ZIPCODE</label>
+            <input type="number" id="zipcode" ref="zipcode" pattern='[0-9]{5}' required/>
+          </div>
+        </div>
+
+        <div className="signup-group2">
+          <div className="input-wrapper">
+            <label htmlFor="email">EMAIL</label>
+            <input type="email" id="email" required/>
+          </div>
+
+          <div className="input-wrapper">
+            <label htmlFor="password">PASSWORD</label>
+            <input type="password" id="password" required/>
+          </div>
+
+          <div className="input-wrapper">
+            <label htmlFor="password-confirm">CONFIRM PASSWORD</label>
+            <input type="password" id="password-confirm" ref="confirmPassword" required/>
+          </div>
+        </div>
+
         <button type="submit" id="signup-submit">SUBMIT</button>
       </form>
     );
     if (this.props.method === 'login') {
       form = (
         <form className="login-form" onSubmit={this.login.bind(this)}>
+
+          <h3>LOGIN</h3>
+
+          <div className="input-wrapper">
           <label htmlFor="email">EMAIL</label>
-          <input type="email" id="email" ref="email"/>
+          <input type="email" id="email" ref="email" required/>
+          </div>
+
+          <div className="input-wrapper">
           <label htmlFor="password">PASSWORD</label>
-          <input type="password" id="password" ref="password"/>
+          <input type="password" id="password" ref="password" required/>
+          </div>
+
           <button type="submit" id="login-submit">LOGIN</button>
         </form>
       );
