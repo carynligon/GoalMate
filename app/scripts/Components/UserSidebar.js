135de0dfc7c26f794f1cc54746c9ba4d759b0412
Caryn Ligon <carynligon@gmail.com>
2016-10-22 15:12:12 -0500

    general layout of user sidebar done

--- app/scripts/Components/UserSidebar.js
+++ app/scripts/Components/UserSidebar.js
@@ -5,8 +5,21 @@ class UserSidebar extends React.Component {
     super(props)
   }
   render() {
+    console.log(this.props);
+    let groups = this.props.groups.map((group,i) => {
+      return (<li key={i}>{group}</li>);
+    });
     return (
       <aside>
+        <div className="user-icon">
+          <img src="/assets/images/user-icon.png"/>
+        </div>
+        <h4>{this.props.user.first_name} {this.props.user.last_name}</h4>
+        <p>{this.props.user.city}</p>
+
+        <div className="user-groups">
+          <ul>{groups}</ul>
+        </div>
       </aside>
     );
   }
