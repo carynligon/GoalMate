23a58018a3dcc9cf24f82e21af5c1f72763adac9
Caryn Ligon <carynligon@gmail.com>
2016-10-22 20:01:17 -0500

    rendering dropdown properly

--- app/scripts/Components/Nav.js
+++ app/scripts/Components/Nav.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 class Nav extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {dropdown: false};
+  }
+  dropdown() {
+    this.setState({dropdown: !this.state.dropdown});
   }
   render() {
+    console.log(this.state);
+    let dropdown;
+    if (this.state.dropdown) {
+      dropdown = (
+        <ul className="nav-dropdown">
+          <li>Profile</li>
+          <li>Add Goal</li>
+          <li>Settings</li>
+          <li>Logout</li>
+        </ul>
+      );
+    }
     return (
       <nav>
+        <Link to="/"><i className="fa fa-home" aria-hidden="true"></i></Link>
+        <i className="fa fa-user user-icon" aria-hidden="true" onClick={this.dropdown.bind(this)}></i>
+        {dropdown}
       </nav>
     );
   }
